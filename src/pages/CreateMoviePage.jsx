895cbf0c742a0b406b25f11aa46029e2a29f5144
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-01-29 17:36:24 +0100

    feat: consegna bonus

--- src/pages/CreateMoviePage.jsx
+++ src/pages/CreateMoviePage.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const CreateMoviePage = () => {
+function CreateMoviePage () {
     const apiUrl = import.meta.env.VITE_API_URL;
 
   const defalutForm = {
